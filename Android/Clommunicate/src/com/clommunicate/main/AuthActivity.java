cfd2885f7388fc12c87742c8b10a14328edcab9e
kemonoske <kemonoske@gmail.com>
2012-11-19 12:33:49 +0200

    Added my project to git

---
+++ Android/Clommunicate/src/com/clommunicate/main/AuthActivity.java
@@ -0,0 +1,163 @@
+package com.clommunicate.main;
+
+import com.clommunicate.main.R;
+import com.clommunicate.utils.Login;
+import com.clommunicate.utils.WebApi;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AuthActivity extends Activity {
+
+	private ImageButton expand_button = null;
+	private ImageButton exit_button = null;
+	private ImageButton add_account_button = null;
+	private ListView accounts_list = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_auth);
+
+		expand_button = (ImageButton) findViewById(R.id.loginButton);
+		exit_button = (ImageButton) findViewById(R.id.exitButton);
+		add_account_button = (ImageButton) findViewById(R.id.auth_add_account_button);
+		accounts_list = (ListView) findViewById(R.id.authAcountList);
+
+		/* Setam fontul etichetei login */
+		Typeface type = Typeface.createFromAsset(getAssets(),
+				"fonts/zekton.ttf");
+		((TextView) findViewById(R.id.authLabel)).setTypeface(type);
+		type = null;
+
+		/* Atribuim actiune la click pentru butonul ok */
+		expand_button.setOnClickListener(new OnClickListener() {
+
+			boolean ex = false;
+
+			public void onClick(View v) {
+				LinearLayout ll = (LinearLayout) findViewById(R.id.midFrame);
+
+				if (!ex) {
+					ll.setVisibility(LinearLayout.VISIBLE);
+
+					String[] tokens = Login.getDeviceAccounts(getBaseContext());
+					String[] values = new String[tokens.length];
+					for (int i = 0; i < tokens.length; i++)
+						values[i] = (tokens[i].split("@"))[0];
+					ArrayAdapter<String> adapter = new AcountArrayAdapter(
+							getBaseContext(), values, tokens);
+					accounts_list.setAdapter(adapter);
+					accounts_list.setVisibility(ListView.VISIBLE);
+
+					((LinearLayout) findViewById(R.id.botChoice))
+							.startAnimation(AnimationUtils.loadAnimation(
+									ll.getContext(), R.anim.auth_bot_frame));
+
+					((LinearLayout) findViewById(R.id.midFrame))
+							.startAnimation(AnimationUtils.loadAnimation(
+									ll.getContext(), R.anim.auth_mid_frame));
+
+					ex = true;
+				} else {
+					ll.setVisibility(LinearLayout.GONE);
+					((ListView) findViewById(R.id.authAcountList))
+							.setVisibility(ListView.GONE);
+					ex = false;
+				}
+			}
+		});
+
+		/* Actiunea pentru butonul cancel */
+		exit_button.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				moveTaskToBack(true);
+			}
+		});
+
+		/* Actiunea pentru butonul de adaugare a acountului */
+		add_account_button.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+
+				findViewById(R.id.loginButton).performClick();
+				startActivity(new Intent(Settings.ACTION_ADD_ACCOUNT));
+
+			}
+		});
+
+		/* Actiunea la tastarea unui account din lista */
+
+		accounts_list.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				final String s = (String) ((TextView) view
+						.findViewById(R.id.auth_acount_item2)).getText();
+				final View v = view;
+				Runnable r = new Runnable() {
+
+					public void run() {
+
+						if (WebApi.login(s)) {
+							
+							
+
+						} else {
+
+							/*
+							 * Daca emailul nu este in baza de date atunci facem
+							 * redirect la activitatea de inregistrare
+							 */
+							Intent i = new Intent(getApplicationContext(),
+									RegistrationActivity.class);
+							i.putExtra("email", s);
+							startActivity(i);
+
+						}
+					}
+				};
+
+				WaitDialog wd = new WaitDialog(view.getRootView().getContext());
+				wd.setTitle(String.format("%100s", "User Authentification..."));
+				wd.show();
+				Thread t = new Thread(r);
+				t.start();
+				while (t.isAlive())
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				wd.dismiss();
+
+			}
+
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_auth, menu);
+		return true;
+	}
+
+}
---
+++ Android/Clommunicate/src/com/clommunicate/main/RegistrationActivity.java
@@ -0,0 +1,152 @@
+package com.clommunicate.main;
+
+import java.io.IOException;
+
+import com.clommunicate.main.R;
+import com.clommunicate.oAuth2.AuthUtils;
+import com.clommunicate.oAuth2.GoogleAccountManager;
+import com.clommunicate.utils.User;
+import com.clommunicate.utils.WebApi;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RegistrationActivity extends Activity {
+	private TextView name = null;
+	private TextView email = null;
+	private TextView locale = null;
+	private TextView gender = null;
+	private ImageView picture = null;
+	private WaitDialog wd = null;
+	private Activity me = this;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		
+
+		name = (TextView) findViewById(R.id.reg_nume);
+		email = (TextView) findViewById(R.id.reg_email);
+		locale = (TextView) findViewById(R.id.reg_location);
+		gender = (TextView) findViewById(R.id.reg_gender);
+		picture = (ImageView) findViewById(R.id.reg_foto);
+		
+		/* Setam fontul etichetei login */
+		Typeface type = Typeface.createFromAsset(getAssets(),
+				"fonts/zekton.ttf");
+		((TextView) findViewById(R.id.regLabel)).setTypeface(type);
+
+		/* Setam fontul la restul etichetelor */
+		type = Typeface.createFromAsset(getAssets(), "fonts/asen.ttf");
+		((TextView) findViewById(R.id.reg_name_label)).setTypeface(type);
+		((TextView) findViewById(R.id.reg_email_label)).setTypeface(type);
+		((TextView) findViewById(R.id.reg_location_label)).setTypeface(type);
+		((TextView) findViewById(R.id.reg_gender_label)).setTypeface(type);
+		((TextView) findViewById(R.id.reg_photo_label)).setTypeface(type);
+		((EditText) findViewById(R.id.reg_nume)).setTypeface(type);
+		((EditText) findViewById(R.id.reg_email)).setTypeface(type);
+		((EditText) findViewById(R.id.reg_location)).setTypeface(type);
+		((EditText) findViewById(R.id.reg_gender)).setTypeface(type);
+
+		ImageButton ib = (ImageButton) findViewById(R.id.registerButton);
+
+		ib.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				WaitDialog wd = new WaitDialog(v.getRootView().getContext());
+				wd.setTitle(String.format("%100s", "User registration."));
+				wd.show();
+			}
+
+		});
+	
+		AsyncTask<String, Void, String> load_data = new AsyncTask<String, Void, String>() {
+			
+			private User usr = null;
+			
+		    @Override
+		    protected void onPreExecute() {
+		        super.onPreExecute();
+				wd = new WaitDialog(me);
+				wd.setTitle(String.format("%100s","Loading oAuth information..."));
+				wd.show();
+		    }
+
+		    @Override
+		    protected String doInBackground(String... params) {
+
+		    	GoogleAccountManager googleAccountManager = new GoogleAccountManager(
+						me);
+				AccountManager am = AccountManager.get(getBaseContext());
+				Account[] accounts = am.getAccountsByType("com.google");
+
+				Account acc = null;
+				
+				for(Account i : accounts)	{
+					
+					if(i.name.equalsIgnoreCase(getIntent().getExtras().getString("email")))	{
+						
+						acc = i;
+						break;
+						
+					}
+				}
+				
+				AuthUtils.refreshAuthToken(me, acc);
+				
+				System.err.println(acc.name);
+				SharedPreferences settings = getSharedPreferences("Clommunicate", 0);
+				String accessToken = settings.getString("accessToken", "");
+				
+				if(!accessToken.equals(""))	{
+					System.err.println(accessToken);
+			        usr = WebApi.getGoogleUser(accessToken,acc.name);
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+		    	
+		    }
+
+		    @Override
+		    protected void onPostExecute(String result) {
+		        super.onPostExecute(result);
+				name.setText(usr.getName());
+				email.setText(usr.getEmail());
+				locale.setText(usr.getLocale());
+				gender.setText(usr.getGender().toString());
+				locale.setText(usr.getLocale());
+				picture.setImageBitmap(usr.getPicture());
+		        wd.dismiss();
+		    }
+		};
+		
+		load_data.execute(null);
+		
+	}
+
+	
+	
+
+	
+}
